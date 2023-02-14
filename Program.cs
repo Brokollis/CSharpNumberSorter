0cbb16e7375e351221171d9511d4920f85a27d82
brokollis <vitor22aguiar06@gmail.com>
2023-02-13 21:59:11 -0300

    Finalização do programa

--- Program.cs
+++ Program.cs
@@ -1,2 +1,64 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace getOrderedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Leitura dos numeros dentro do arquivo de texto:
+            
+            string[] lines = File.ReadAllLines("unordered_values.txt");
+            int[] numbers = Array.ConvertAll(lines, int.Parse);
+            
+            Console.WriteLine("Numeros obtidos do arquivo de texto:");
+            Console.WriteLine(string.Join(", ", numbers));
+            
+            // Ordenação dos numeros dentro do vetor:
+
+            int auxNumber, minNumber;
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                minNumber = i;
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[j] < numbers[minNumber])
+                    {
+                        minNumber = j;
+                    }
+                }
+                if (minNumber != i)
+                {
+                    auxNumber = numbers[i];
+                    numbers[i] = numbers[minNumber];
+                    numbers[minNumber] = auxNumber;
+                }
+            }
+            
+            Console.WriteLine("Numeros obtidos no vetor:");
+            Console.WriteLine(string.Join(", ", numbers));
+            
+            // Criação de uma lista com os numeros obtidos no vetor em ordem
+            var sortedNumber = numbers.ToList();
+            
+            Console.WriteLine("Numeros ordenados e alocados no novo arquivo de texto:");
+            Console.WriteLine(string.Join(", ", sortedNumber));
+            
+            // Verificação dos valores na lista e no vetor. Se forem iguais, escreve na tela que são iguais, se não, escreve que são diferentes.
+            if (numbers.SequenceEqual(sortedNumber))
+            {
+                Console.WriteLine("Os valores presentes na lista e no vetor são iguais!");
+            }
+            else
+            {
+                Console.WriteLine("Os valores presentes na lista e no vetor são diferentes!");
+            }
+            
+            // Insere os numeros já ordenados em um arquivo de texto secundário.
+            File.WriteAllLines("ordered_values.txt", sortedNumber.Select(x => x.ToString()));
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# estruturas-de-Array-em-C-
-Prova C# 13/02
+# Ordenação de valores 
+
+O Projeto tem como objetivo ler um arquivo de texto, que por sua vez possui numeros que podem estar ou não ordenados. Após a leitura do arquivo de texto, o programa obtem os numeros e os ordena através de um vetor que imprime os valores na tela.
+
+### 📋 Pré-requisitos
+
+<strong>Visual Studio Code</strong>: você pode baixar e instalar a última versão do VSCode a partir do site oficial.
+
+<strong>.NET SDK (Software Development Kit)</strong>: você precisa instalar a última versão do .NET SDK para desenvolver aplicativos em C#. Você pode baixar o .NET SDK a partir do site da Microsoft.
+
+<strong>Extensão de C# para o Visual Studio Code</strong>: para escrever, depurar e executar código em C# no VSCode, você precisa instalar a extensão de C#. Você pode encontrar a extensão no marketplace de extensões do VSCode.
+
+### 🔧 Instalação
+
+Após os pré-requisitos listados acima serem atendidos, abra o terminal do vs code e digite <strong>"dotnet run"</strong> para que a aplicação seja executada 
+
+Termine com um exemplo de como obter dados do sistema ou como usá-los para uma pequena demonstração.
+
+## 🛠️ Construído com
+
+Linhaguem de programação C#;
+Arquivo de texto
+
+---
+⌨️ com ❤️ por [Armstrong Lohãns](https://gist.github.com/brokollis) 😊
